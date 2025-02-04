e56832280101d81db74a227ab265aff06d681754
krishnarajuy <krishnaraju6302@gmail.com>
2025-02-04 14:40:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
+count = 3
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "my1st server"
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["ansible", "dev", "test"]
 }
